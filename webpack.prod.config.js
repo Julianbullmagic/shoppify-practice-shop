04ee8a962a88d400da28eccec71fdecde71fd870
Julian Bull <julianbullmagic@gmail.com>
2022-01-13 10:10:45 +1100

    fff

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -15,12 +15,12 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   minimize: true,
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
